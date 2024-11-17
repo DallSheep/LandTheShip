7c83e1ca2d1dd443dfe79b3e11d779c3da67eb70
christian dall <Christian.j.dall@gmail.com>
2024-11-17 12:51:51 -0500

    CD: UI clean up

---
+++ LandTheShip/Source/LandTheShip/LevelSelect.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WidgetBlueprint.h"
+#include "LevelSelect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LANDTHESHIP_API ULevelSelect : public UWidgetBlueprint
+{
+	GENERATED_BODY()
+	
+};
